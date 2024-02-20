01ac4d683c3bf8df57af48d2da0b6be8859dec94
Elizabeth <elizabethmahima235@gmail.com>
2024-02-20 16:56:14 +0400

    >creating member edit component >adding update functionality >adding canDeactivate routegaurd >persisting changes in API >using memberservice to store state

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import {TabsModule} from 'ngx-bootstrap/tabs'
+import { NgxSpinner, NgxSpinnerModule } from 'ngx-spinner';
 
 
 
@@ -14,13 +15,17 @@ import {TabsModule} from 'ngx-bootstrap/tabs'
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       positionClass:'toast-bottom-right'
-    }) // ToastrModule added
+    }), // ToastrModule added
+    NgxSpinnerModule.forRoot({
+      type: 'ball-spin-clockwise-fade-rotating'
+    })
   ],
 
   exports:[
     BsDropdownModule,
     ToastrModule,
-    TabsModule
+    TabsModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
